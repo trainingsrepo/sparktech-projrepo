665ce1ffed7a1b059b8a2ef7cfbf8208a8c460a8
trainingsrepo <trainings.anil@gmail.com>
2026-09-08 10:47:07 +0530

    email validation

--- dev-validation.js
+++ dev-validation.js
@@ -1,3 +1,15 @@
 validation of the user
 using javascript
 code here...
+function validationForm(){
+  
+          var name=document.getElementById("name").value;
+		  var email=document.getElementById("email").value;
+		  var Password=document.getElementById("Password").value;
+		  var confirmPassword=document.getElementById("confirmPassword").value;
+	
+	#new modifications @11:30am
+	       if(name == ""){
+			   alert ("Name field must not be empty, Please enter the value")
+			   return false
+}
